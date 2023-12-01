d9a890a38dd730df33ef2b54865a16181662e10b
eliasantana <eliasantanasilva@gmail.com>
2023-11-30 21:36:04 -0300

    feature/#v.1.1 | Adicionando a classe AlunoController, AlunoRepository, Aluno, AlunoServices

---
+++ src/main/java/br/com/escola/api/repository/AlunoRepository.java
@@ -0,0 +1,9 @@
+package br.com.escola.api.repository;
+
+import br.com.escola.api.model.Aluno;
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface AlunoRepository extends CrudRepository<Aluno, Long> {
+}
