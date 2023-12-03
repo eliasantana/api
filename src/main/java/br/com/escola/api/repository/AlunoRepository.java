dd08a2144f1c46e16c94cc237da1ddd1566fe1e0
eliasantana <eliasantanasilva@gmail.com>
2023-12-03 10:17:13 -0300

    feature/#v.1.3 | Adicionando CRUD para a entidade Aluno

--- src/main/java/br/com/escola/api/repository/AlunoRepository.java
+++ src/main/java/br/com/escola/api/repository/AlunoRepository.java
@@ -1,9 +1,19 @@
 package br.com.escola.api.repository;
 
+import br.com.escola.api.dto.AlunoDto;
 import br.com.escola.api.model.Aluno;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface AlunoRepository extends CrudRepository<Aluno, Long> {
+    @Query(value = "select * from aluno where cd_aluno=:id", nativeQuery = true)
+    Aluno localizarAluno(Long id);
+
+    @Query(value = "Select * from aluno",nativeQuery = true)
+    List<Aluno> listarTodos();
 }
+
