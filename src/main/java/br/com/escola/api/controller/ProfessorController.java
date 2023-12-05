91348c475f5a3b1d1763ee243e31e37cbe536d71
eliasantana <eliasantanasilva@gmail.com>
2023-12-04 23:15:04 -0300

    bugfix/#v.1.4 | corrigindo commit anterior

--- src/main/java/br/com/escola/api/controller/ProfessorController.java
+++ src/main/java/br/com/escola/api/controller/ProfessorController.java
@@ -1,2 +1,42 @@
-package br.com.escola.api.controller;public class ProfessorController {
+package br.com.escola.api.controller;
+
+import br.com.escola.api.dto.ProfessorDto;
+import br.com.escola.api.services.ProfessorServices;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
+import org.springframework.stereotype.Service;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/professor")
+public class ProfessorController {
+    @Autowired
+    ProfessorServices services;
+
+    @GetMapping("/listar")
+    public ResponseEntity<List<ProfessorDto>> listarTodos(){
+       return services.listarTodos();
+    }
+
+    @GetMapping("/localizar/{id}")
+    public ResponseEntity<ProfessorDto>localizar(@PathVariable Long id ){
+        return services.localizar(id);
+    }
+    @DeleteMapping("/delete/{id}")
+    public ResponseEntity<ProfessorDto>delete(@PathVariable Long id ){
+        return services.delete(id);
+    }
+    @PostMapping("/adicionar")
+    public ResponseEntity<ProfessorDto>create(@RequestBody ProfessorDto dto){
+        return services.adicionar(dto);
+    }
+    @PutMapping("/alterar/{id}")
+    public ResponseEntity<ProfessorDto>create(@RequestBody ProfessorDto dto, @PathVariable Long id){
+        return services.alterar(dto, id);
+    }
+
+
 }
--- src/main/java/br/com/escola/api/dto/ProfessorDto.java
+++ src/main/java/br/com/escola/api/dto/ProfessorDto.java
@@ -1,2 +1,34 @@
-package br.com.escola.api.dto;public class ProfessorDto {
+package br.com.escola.api.dto;
+
+import br.com.escola.api.model.Professor;
+import jakarta.validation.constraints.NotNull;
+
+public class ProfessorDto {
+
+    private Long cdProfessor;
+    @NotNull(message = "O campo nome é obrigatório")
+    private String nome;
+
+    public ProfessorDto(){}
+
+    public ProfessorDto(Professor professor){
+        this.cdProfessor = professor.getCdProfessor();
+        this.nome = professor.getNome();
+    }
+
+    public Long getCdProfessor() {
+        return cdProfessor;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    @Override
+    public String toString() {
+        return "ProfessorDto{" +
+                "cdProfessor=" + cdProfessor +
+                ", nome='" + nome + '\'' +
+                '}';
+    }
 }
